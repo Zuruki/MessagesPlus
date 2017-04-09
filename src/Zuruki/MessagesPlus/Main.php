8a9becf85ee2b2b250c33a3756435483b5070e14
Zuruki <zurukixeo@gmail.com>
2017-04-09 22:47:43 +0100

    Update Main.php

--- src/Zuruki/MessagesPlus/Main.php
+++ src/Zuruki/MessagesPlus/Main.php
@@ -1,1 +1,31 @@
+<?php
+
+namespace Zuruki\MessagesPlus;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener {
+	public function onEnable(){
+	$this->getLogger()->info(" Enabled!");
+	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	@mkdir($this->getDataFolder());
+        $this->cfg = $this->getConfig();
+        $this->saveDefaultConfig();
+	}
+	public function onDisable(){
+	$this->getLogger()->info(" Enabled!");
+	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function onJoin(PlayerJoinEvent $ejoin){
+	$joinmsg = $this->cfg->get("JoinMessage:");
+	$player = $ejoin->getPlayer();
+	$name = $ejoin->getPlayer()->getName();
+	$this->getServer()->broadcastMessage($joinmsg);
+	}
+	
+
+
 
